d9ff00d0113d26bb496652374face57ffe14a70f
Shkorio <Student@CAB4-2>
2023-04-10 13:16:26 +1000

    Добавьте файлы проекта.

---
+++ Galery/Input.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Galery
+{
+    /// <summary>
+    /// Логика взаимодействия для Input.xaml
+    /// </summary>
+    public partial class Input : Window
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public Input()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void buttonAuth(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void buttonSignIn(object sender, RoutedEventArgs e)
+        {
+            MainWindow mv = new MainWindow();
+            mv.Show();
+            Close();
+        }
+    }
+}
